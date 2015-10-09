a09fe016a3fcad7fc73df613d574bb7b1a5a4ff6
cvj3 <cvj3@cornell.edu>
2015-10-09 00:04:19 -0400

    placeholder for parser for data files

---
+++ parseData.py
@@ -0,0 +1,13 @@
+# not implemented!!
+# accept 'test' or 'train' (or just pure filename?)
+# convert data/training-data.data into a dictionary object
+# NOTE: will need to wrap <dictionary> + </dictionary> to data files before parsing
+
+# training_set [ list of ????
+#                {
+#                  "word": ''
+#                  , "type": ''
+#                  , "context": ''
+#                  , "answer_id": ''
+#                }
+# ]
