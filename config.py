341acd4b32dcbc0d57227b813dc36db5990cf9ae
Curt Josey <cvj3@cornell.edu>
2015-10-22 14:16:34 -0400

    code cleanup2

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # if True uses context from immediate sentence, else uses ALL words
-TEST_BY_SENTENCE = True
+TEST_BY_SENTENCE = False
 
 # if True uses 100% data, else 75% training and then tests on 25%
 WRITE_TEST = False
